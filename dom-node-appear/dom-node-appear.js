605d04dda800055153b74153f1049b455eda9d45
Douglas Hipolito <me@douglashipolito.com>
2014-03-28 01:58:49 -0300

    adding comments

--- dom-node-appear/dom-node-appear.js
+++ dom-node-appear/dom-node-appear.js
@@ -1,47 +1,67 @@
 xui.extend({
-		
-    appears: function(callback) {
-        var self = this,
-        		head = x$('head'),
-        		
-        		//Options of animation
-        		options = {
-		      		keyframes: "@keyframes nodeInserted {from {clip: rect(1px, auto, auto, auto); } to {clip: rect(0px, auto, auto, auto); } } @-moz-keyframes nodeInserted {from {clip: rect(1px, auto, auto, auto); } to {clip: rect(0px, auto, auto, auto); } } @-webkit-keyframes nodeInserted {from {clip: rect(1px, auto, auto, auto); } to {clip: rect(0px, auto, auto, auto); } } @-ms-keyframes nodeInserted {from {clip: rect(1px, auto, auto, auto); } to {clip: rect(0px, auto, auto, auto); } } @-o-keyframes nodeInserted {from {clip: rect(1px, auto, auto, auto); } to {clip: rect(0px, auto, auto, auto); } }, ",
-		      		selector: self.selector,
-		      		stylesClass: self.selector.replace(".", ""),
-		      		styles: self.selector + " { animation-name: nodeInserted; -webkit-animation-name: nodeInserted; animation-duration: 0.001s; -webkit-animation-duration: 0.001s; }"
-		    		},
+		/**
+		 * 
+		 * This executes a callback only when the element appears on the DOM
+		 * 
+		 * @param {Function} callback Function that will be called when the element appears on the DOM. 
+		 * @return self
+		 * @example
+		 * 
+		 * ### appears 
+		 *	
+		 * syntax:
+		 *
+		 * 		appears(callback);
+		 * 
+		 * example: 
+		 * 
+		 * 		x$('#node').appears(function () {
+		 *			//do something
+		 *		});
+		 * 
+		*/
+		appears: function(callback) {
+			var self = this,
+					head = x$('head'),
 
-		    		//list of events
-		    		events = 'animationstart webkitAnimationStart oanimationstart MSAnimationStart'.split(' '),
+					//Options of animation
+					options = {
+						keyframes: "@keyframes nodeInserted {from {clip: rect(1px, auto, auto, auto); } to {clip: rect(0px, auto, auto, auto); } } @-moz-keyframes nodeInserted {from {clip: rect(1px, auto, auto, auto); } to {clip: rect(0px, auto, auto, auto); } } @-webkit-keyframes nodeInserted {from {clip: rect(1px, auto, auto, auto); } to {clip: rect(0px, auto, auto, auto); } } @-ms-keyframes nodeInserted {from {clip: rect(1px, auto, auto, auto); } to {clip: rect(0px, auto, auto, auto); } } @-o-keyframes nodeInserted {from {clip: rect(1px, auto, auto, auto); } to {clip: rect(0px, auto, auto, auto); } }, ",
+						selector: self.selector,
+						stylesClass: self.selector.replace(".", ""),
+						styles: self.selector + " { animation-name: nodeInserted; -webkit-animation-name: nodeInserted; animation-duration: 0.001s; -webkit-animation-duration: 0.001s; }"
+					},
 
-		    		//Callback of dom node appear
-		    		animationStart = function (event) {
-		    			var target = x$(event.target),
-		    					targetSelector = x$(options.selector);
+					//list of events
+					events = 'animationstart webkitAnimationStart oanimationstart MSAnimationStart'.split(' '),
 
-		    			targetSelector = targetSelector[targetSelector.length -1];
+					//Callback of dom node appear
+					animationStart = function (event) {
+						var target = x$(event.target),
+						targetSelector = x$(options.selector);
 
-				      if ((event.animationName === 'nodeInserted' && target[0] === targetSelector) && typeof callback === 'function') {
-				      	callback.call(target[0], target);
-				      }
-		    		};
+						targetSelector = targetSelector[targetSelector.length -1];
 
-		    //if the keyframes aren't present, add them in a style element
-		    if(!x$("style.domnodeappear-keyframes").length) {
-		      head.bottom("<style class='domnodeappear-keyframes'>" + options.keyframes + "</style>");
-		    }
+						if ((event.animationName === 'nodeInserted' && target[0] === targetSelector) && typeof callback === 'function') {
+							callback.call(target[0], target);
+						}
+					};
 
-		    //add animation to selected element
-		    head.bottom("<style class=\"" + options.stylesClass + "-animation\">" + options.styles + "</style>")
+			//if the keyframes aren't present, add them in a style element
+			if(!x$("style.domnodeappear-keyframes").length) {
+				head.bottom("<style class='domnodeappear-keyframes'>" + options.keyframes + "</style>");
+			}
 
-		    //Attaches events to the document
-		    events.forEach(function (eventString) {
-		    	x$(document).on(eventString, animationStart);
-		    });
+			//add animation to selected element
+			head.bottom("<style class=\"" + options.stylesClass + "-animation\">" + options.styles + "</style>")
 
-		    return this;
-    }
+			//Attaches events to the document
+			events.forEach(function (eventString) {
+				x$(document).on(eventString, animationStart);
+			});
+
+			return this;
+		}
 });
 
 //It just exists to get the selector
