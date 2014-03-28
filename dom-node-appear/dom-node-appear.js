cf78e239c734491a2bfbc43c9acd42f5c96a9c6c
Douglas Hipolito <me@douglashipolito.com>
2014-03-28 01:54:47 -0300

    catching the selector through of method find

--- dom-node-appear/dom-node-appear.js
+++ dom-node-appear/dom-node-appear.js
@@ -6,3 +6,14 @@ xui.extend({
 		    return this;
     }
 });
+
+//It just exists to get the selector
+//It saves the reference of the xui and replaces the main "find" in order to save the selector
+(function (window) {
+	var _xui = xui;
+	window.x$ = window.xui = xui = function(q, context) {	
+			var Xui = new _xui.fn.find(q, context);
+			Xui.selector = q;
+	    return Xui;
+	};
+}(window));
