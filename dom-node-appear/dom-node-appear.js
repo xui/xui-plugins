86c7260f475d19ca3c0b6673a4ce54da4dd3e637
Douglas Hipolito <me@douglashipolito.com>
2014-03-28 03:00:39 -0300

    dom-node-appear - fixing the problem  with object inheritance

--- dom-node-appear/dom-node-appear.js
+++ dom-node-appear/dom-node-appear.js
@@ -68,9 +68,14 @@ xui.extend({
 //It saves the reference of the xui and replaces the main "find" in order to save the selector
 (function (window) {
 	var _xui = xui;
+
 	window.x$ = window.xui = xui = function(q, context) {	
-			var Xui = new _xui.fn.find(q, context);
-			Xui.selector = q;
+		var Xui = new _xui.fn.find(q, context);
+		Xui.selector = q;
 	    return Xui;
 	};
+	
+	xui.fn = xui.prototype = _xui.prototype;
+	xui.fn.find.prototype = _xui.fn;
+	xui.extend = _xui.fn.extend;
 }(window));
